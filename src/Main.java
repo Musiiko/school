feffb2033852c7c381c74b7fde4d9612998c9115
MSP1981 <99791179+MSP1981@users.noreply.github.com>
2022-07-03 15:38:53 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner score = new Scanner(System.in);
+        System.out.print("Enter a score on a twelve-point scale  = ");
+        int number = score.nextInt();
+        switch (number){
+            case 1:
+            case 2:
+            case 3:{
+                System.out.println("Your score is two");
+                break;
+            }
+            case 4:
+            case 5:
+            case 6:{
+                System.out.println("Your score is three");
+                break;
+            }
+            case 7:
+            case 8:
+            case 9:{
+                System.out.println("Your score is four");
+                break;
+            }
+            case 10:
+            case 11:
+            case 12:{
+                System.out.println("Your score is five");
+                break;
+            }
+            default:{
+                System.out.println("Error . There is no such rating. Enter again.");
+            }
+        }
     }
 }
